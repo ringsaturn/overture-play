17cbd1a8febae163506615c63d51df91840a0e2d
ringsaturn <ringsaturn.me@gmail.com>
2023-10-12 13:52:56 +0800

    Update queries

--- queries/admin.sql
+++ queries/admin.sql
@@ -1,5 +1,5 @@
 LOAD spatial;
--- Full
+--- Process all
 COPY (
     SELECT type,
         subType,
@@ -15,7 +15,7 @@ COPY (
             hive_partitioning = 1
         )
     WHERE ST_GeometryType(ST_GeomFromWkb(geometry)) IN ('POLYGON', 'MULTIPOLYGON')
-) TO 'countries_all_levels.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
+) TO 'countries.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
 -- Process level 2
 COPY (
     SELECT type,
@@ -33,22 +33,4 @@ COPY (
         )
     WHERE adminLevel = 2
         AND ST_GeometryType(ST_GeomFromWkb(geometry)) IN ('POLYGON', 'MULTIPOLYGON')
-) TO 'countries.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
--- Process level 4
-COPY (
-    SELECT type,
-        subType,
-        localityType,
-        adminLevel,
-        isoCountryCodeAlpha2,
-        JSON(names) AS names,
-        JSON(sources) AS sources,
-        ST_GeomFromWkb(geometry) AS geometry
-    FROM read_parquet(
-            './type=*/*',
-            filename = true,
-            hive_partitioning = 1
-        )
-    WHERE adminLevel = 4
-        AND ST_GeometryType(ST_GeomFromWkb(geometry)) IN ('POLYGON', 'MULTIPOLYGON')
-) TO 'countries_level4.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
+) TO 'countries_level_2.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
